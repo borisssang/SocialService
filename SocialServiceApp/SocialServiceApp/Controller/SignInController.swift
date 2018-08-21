d7db8dd43b604ed5afa5f0cc3c4e168ed66b5b2d
borisssang <borisssang@gmail.com>
2018-08-21 19:33:27 +0300

    Added controller for signing in, a ProgressHUD

---
+++ SocialServiceApp/SocialServiceApp/Controller/SignInController.swift
@@ -0,0 +1,9 @@
+//
+//  SignInController.swift
+//  SocialServiceApp
+//
+//  Created by Boris Angelov on 21.08.18.
+//  Copyright © 2018 Boris Angelov. All rights reserved.
+//
+
+import Foundation
